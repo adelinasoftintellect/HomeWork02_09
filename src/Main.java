56daaf7dc88b0002088045bd78a97019e4a4983c
Adi <adelina_yordanova11@abv.bg>
2016-05-21 13:50:34 +0300

    HomeWork02_09.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 14;
+		System.out.print("a: ");
+		System.out.println(a);
+
+		int b = 10;
+		System.out.print("b: ");
+		System.out.println(b);
+
+		int c = a % b;
+		System.out.println("(a % b) = " + c);
+
+	}
+
+}
